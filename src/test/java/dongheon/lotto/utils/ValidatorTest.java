a97b280513261631962ec3ed58172a57c1a5dc60
opdshe <50160282+opdshe@users.noreply.github.com>
2020-04-29 15:39:14 +0900

    test : 구입 금액 검증 테스트코드 추가

--- src/test/java/dongheon/lotto/utils/ValidatorTest.java
+++ src/test/java/dongheon/lotto/utils/ValidatorTest.java
@@ -6,6 +6,34 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ValidatorTest {
-   
+    @Test
+    void 구입금액_숫자입력_확인() {
+        //given
+        String testPrice = "천원";
+        //when
+        boolean testResult = Validator.checkTotalPrice(testPrice);
+        // then
+        assertThat(testResult).isFalse();
+    }
+
+    @Test
+    void 구입금액_범위_확인() {
+        // given
+        String testPrice = "-1000";
+        // when
+        boolean testResult = Validator.checkTotalPrice(testPrice);
+        // then
+        assertThat(testResult).isFalse();
+    }
+
+    @Test
+    void 구입금액_단위_확인() {
+        // given
+        String testPrice = "1500";
+        // when
+        boolean testResult = Validator.checkTotalPrice(testPrice);
+        // then
+        assertThat(testResult).isFalse();
+    }
 
 }
